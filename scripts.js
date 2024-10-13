23d64cf08302393b59927d363494778bc4460e11
sskjoldr <evgeniypolitun@gmail.com>
2024-10-13 14:09:55 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// Функция для копирования IP
+function copyToClipboard(text) {
+    const tempInput = document.createElement("input");
+    tempInput.style = "position: absolute; left: -1000px; top: -1000px";
+    tempInput.value = text;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand("copy");
+    document.body.removeChild(tempInput);
+    alert("IP скопирован: " + text);
+}
